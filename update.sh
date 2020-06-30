2a8cc29a2cef8ccc5871619f344ba15952d46866
Gunnar Sundberg <gunnar.sundberg82@gmail.com>
2020-06-30 18:55:59 -0400

    fix symlink

--- update.sh
+++ update.sh
@@ -16,5 +16,5 @@ rm master.zip
 cd $NAME/covid-tracker-dashboard-master
 npm install
 npm run build
-ln -s /current-build ~/covid-tracker-dashboard/$NAME/covid-tracker-dashboard-master/
+ln -sfn ~/covid-tracker-dashboard/$NAME/covid-tracker-dashboard-master/ /current-build
 pm2 reload covid-tracker-dashboard
