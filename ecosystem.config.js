606d7638237cdc1c26a8ea92c37ce2342e0a7c5c
Gunnar Sundberg <gunnar.sundberg82@gmail.com>
2020-06-30 18:13:46 -0400

    new pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   apps : [{
-    name      : 'covidfrontend', // App name that shows in `pm2 ls`
+    name      : 'covid-tracker-dashboard', // App name that shows in `pm2 ls`
     exec_mode : 'cluster', // enables clustering
     instances : '2',
-    //cwd       : './current', // only if using a subdirectory
+    cwd       : '/current-release', // only if using a subdirectory
     script: './node_modules/nuxt-start/bin/nuxt-start.js',
     watch: true,
   }],
