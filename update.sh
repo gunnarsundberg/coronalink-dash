a93f9b03f0550ce1a55ec35db45b11cf5c5374fc
Gunnar Sundberg <gunnar.sundberg82@gmail.com>
2020-06-30 18:06:05 -0400

    update script

---
+++ update.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+while getopts n: option
+do
+case "${option}"
+in
+n) NAME=${OPTARG};;
+esac
+done
+
+wget https://github.com/gunnarsundberg/covid-tracker-dashboard/archive/master.zip
+
+unzip master.zip -d $NAME
+rm master.zip
+
+cd $NAME/covid-tracker-dashboard-master
+npm install
+npm run build
+ln -s /current-build ~/covid-tracker-dashboard/$NAME/covid-tracker-dashboard-master/
+pm2 reload covid-tracker-dashboard
