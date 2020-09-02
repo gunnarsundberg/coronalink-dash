c673bca1dfa161a0534d242e22e24f4e17786e3b
Gunnar Sundberg <gunnar.sundberg82@gmail.com>
2020-09-02 11:50:51 -0400

    use absolute path for cwd

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     name      : 'covid-tracker-dashboard', // App name that shows in `pm2 ls`
     exec_mode : 'cluster', // enables clustering
     instances : '2',
-    cwd       : '/current-build', // only if using a subdirectory
+    cwd       : '~/covid-tracker-dashboard/current-build', // only if using a subdirectory
     script: './node_modules/nuxt-start/bin/nuxt-start.js',
     watch: true,
   }],
